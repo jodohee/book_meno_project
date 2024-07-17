e118863c6f34ae28b23948ee39b117bfaebd8895
jodohee <whehgml1106@gmail.com>
2024-07-17 15:45:25 +0900

    2

---
+++ spring_boot_back/src/main/java/memo_project/spring_boot_back/SpringBootBackApplication.java
@@ -0,0 +1,13 @@
+package memo_project.spring_boot_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackApplication.class, args);
+	}
+
+}
---
+++ spring_boot_back/src/test/java/memo_project/spring_boot_back/SpringBootBackApplicationTests.java
@@ -0,0 +1,13 @@
+package memo_project.spring_boot_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
