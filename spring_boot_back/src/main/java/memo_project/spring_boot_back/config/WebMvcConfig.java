f3c3e0372f1c6ee7077784772b7083002ca62786
jodohee <whehgml1106@.gmail.com>
2024-08-02 15:47:38 +0900

    ok

--- spring_boot_back/src/main/java/memo_project/spring_boot_back/config/WebMvcConfig.java
+++ spring_boot_back/src/main/java/memo_project/spring_boot_back/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("http://localhost:3001") // 허용할 도메인
+                .allowedOrigins("http://localhost:3000","http://localhost:3000") // 허용할 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 인증 정보 포함 허용
